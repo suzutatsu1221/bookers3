11a73f67e333d0c2d9514a883fa349ce8b82331f
suzutatsu <suzuta1221@gmail.com>
2019-08-15 17:00:56 +0900

    [update]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,33 @@
 class BooksController < ApplicationController
+  def top
+  end
+
+
   def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  		format.html {redirect_to @book, notice: 'Successfully'}
   end
 
-  def show
-  end
-
-  def new
+ def show
+ 	@book = Book.find(params[:id])
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
+
+private
+def book_params
+	params.require(:book).permit(:title, :body)
+end
+
+
+
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
+ get '/'  => 'books#top'
+
+ get '/books' => 'books#index'
+ get '/books' => 'books#new'
+ post '/books' => 'books#create'
+ get '/books/:id' => 'books#show'
+ get '/books/:id/edit' => 'books#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
