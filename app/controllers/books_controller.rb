38da15e416e7d3087f0575559152a55ad35c50e5
suzutatsu <suzuta1221@gmail.com>
2019-08-16 17:15:52 +0900

    [finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,14 +31,14 @@ class BooksController < ApplicationController
 def update
   book = Book.find(params[:id])
   book.update(book_params)
-  redirect_to book_path
+  redirect_to book_path(book.id), notice: "successfully"
 end
 
 
 def destroy
         book = Book.find(params[:id])
         book.destroy
-        redirect_to books_path
+        redirect_to "/books"
   end
 
 
